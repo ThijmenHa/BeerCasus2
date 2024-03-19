bbcb3e648f8ed1171e04f9b465bd8ac370e06b92
ThijmenHa <97098523@st.deltion.nl>
2024-03-19 15:20:18 +0100

    Added cookies - only being able to like once

--- index.php
+++ index.php
@@ -3,10 +3,39 @@ include "dbrefs.php";
 
 $con = mysqli_connect($server, $user, $pw, $dbname);
 
-if (mysqli_connect_errno() ) {
+if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
     exit();
 }
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['like'])) {
+    $beers_id = $_POST['beers_id'];
+    
+
+    if (!isset($_COOKIE['liked_beers']) || strpos($_COOKIE['liked_beers'], $beers_id) === false) {
+
+        $query = "UPDATE beers SET like_count = like_count + 1 WHERE id = $beers_id";
+        $result = mysqli_query($con, $query);
+
+        if (isset($_COOKIE['liked_beers'])) {
+            $liked_beers = $_COOKIE['liked_beers'] . ',' . $beers_id;
+        } else {
+            $liked_beers = $beers_id;
+        }
+        setcookie('liked_beers', $liked_beers, time() + (86400 * 30), "/"); 
+        
+        if ($result) {
+            echo "<script>console.log('Je hebt biertje " . $beers_id . " geliked' );</script>";
+            header("Location: ".$_SERVER['PHP_SELF']);
+            exit();
+        } else {
+            echo "<p>Kan biertje niet liken " . mysqli_error($con) . "</p>";
+        }
+    } else {
+        echo "<p>Je hebt dit biertje al geliked.</p>";
+    }
+} 
 
 $sql = "SELECT * FROM beers";
 $result = mysqli_query($con, $sql);
@@ -15,7 +44,6 @@ if (!$result) {
     exit();
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,7 +53,9 @@ if (!$result) {
     <title>Biertjes</title>
 </head>
 <body>
-    <h1>Biertjes</h1>
+    <div class="container">
+
+    <h1>Biertjes Lijst</h1>
     
     <table class='datatable'>
         <tr>
@@ -48,33 +78,20 @@ if (!$result) {
             echo "<td>" . $row['perc'] . "</td>";
             echo "<td>" . $row['purchase_price'] . "</td>";
             echo "<td>" . $row['like_count'] . "</td>";
-            echo "<td>";
-            echo "<form method='post'>";
-            echo "<input type='hidden' name='beers_id' value='" . $row['id'] . "'>";
-            echo "<button type='submit' name='like'>Like</button>";
+            echo "<td>"; 
+            echo "<form method='POST'>";
+            echo "<input type='submit' name='like' value='Like'></input>"; 
+            echo "<input type='hidden' name='beers_id' value='" . $row['id'] . "'>"; 
             echo "</form>";
             echo "</td>";
             echo "</tr>";
         }
         ?>
     </table>
-
+    </div> 
 </body>
 </html>
-        
-    <?php 
-    if (isset($_POST['like'])) {
-        $beers_id = $_POST['beers_id'];
-        $query = "UPDATE beers SET like_count = like_count + 1 WHERE id = $beers_id";
-        $result = mysqli_query($con, $query);
-        if ($result) {
-            echo "<p>beer liked successfully!</p>";
-        } else {
-            echo "<p>Error liking post: " . mysqli_error($con) . "</p>";
-        }
-    }
-    ?>
-    
+
 <?php 
 mysqli_close($con);
 ?>
